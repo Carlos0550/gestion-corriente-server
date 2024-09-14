c50e8edc622c7866eb6d1fdef5866f2ccd1c364a
Loscar01 <carlospelinski03@gmail.com>
2024-09-14 20:23:03 -0300

    Se creo un endpoint para las tareas programadas ya que vercel no permite esto

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const dayjs = require("dayjs");
-const utc = require("dayjs/plugin/utc.js")
-const timezone = require("dayjs/plugin/timezone.js")
+const utc = require("dayjs/plugin/utc.js");
+const timezone = require("dayjs/plugin/timezone.js");
 require("dotenv").config();
 const cron = require("node-cron");
 const clientSupabase = require("./bd/clientSupabase.js");
@@ -15,22 +15,47 @@ app.use(
   })
 );
 app.use(express.json());
-dayjs.extend(utc)
-dayjs.extend(timezone)
-const argTime = dayjs().tz("America/Argentina/Buenos_Aires")
+dayjs.extend(utc);
+dayjs.extend(timezone);
+const argTime = dayjs().tz("America/Argentina/Buenos_Aires");
 
 app.get("/", (req, res) => {
   res.send("Servidor levantado");
 });
-console.log("Fecha de hoy (Argentina): ", argTime.format("YYYY-MM-DD"))
-cron.schedule("14 20 * * *", async () => {
-  console.log("Ejecutando tarea CRON para los vencimientos");
+
+//Cron quitado ya que VERCEL no admite tareas programadas :(
+// cron.schedule("0 0 * * *", async () => {
+//   console.log("Ejecutando tarea CRON para los vencimientos");
+//   try {
+//     const hoy = argTime.format("YYYY-MM-DD");
+//     const query = "UPDATE debts SET estado = 'vencido' WHERE duedate <= $1";
+//     await clientSupabase.query("BEGIN");
+//     const response = await clientSupabase.query(query, [hoy]);
+//     console.log(response)
+//     if (response.rowCount > 0) {
+//       await clientSupabase.query("COMMIT");
+//       console.log(
+//         `Deudas actualizadas exitosamente, ${response.rowCount} filas fueron afectadas`
+//       );
+//     } else {
+//       console.log("No hay deudas para actualizar en este momento.");
+//     }
+//   } catch (error) {
+//     await clientSupabase.query("ROLLBACK");
+//     console.error(
+//       "Hubo un problema y no se pudo concretar la tarea CRON:",
+//       error
+//     );
+//   }
+// });
+
+app.put("/get-expirations", async (req, res) => {
   try {
     const hoy = argTime.format("YYYY-MM-DD");
     const query = "UPDATE debts SET estado = 'vencido' WHERE duedate <= $1";
     await clientSupabase.query("BEGIN");
     const response = await clientSupabase.query(query, [hoy]);
-    console.log(response)
+    console.log(response);
     if (response.rowCount > 0) {
       await clientSupabase.query("COMMIT");
       console.log(
@@ -82,21 +107,17 @@ app.get("/get-all-clients", async (req, res) => {
     if (response.rowCount > 0) {
       return res.status(200).send(response.rows);
     } else {
-      return res
-        .status(500)
-        .json({
-          message: "Error al traer los clientes del servidor",
-          error: response,
-        });
+      return res.status(500).json({
+        message: "Error al traer los clientes del servidor",
+        error: response,
+      });
     }
   } catch (error) {
-    return res
-      .status(500)
-      .json({
-        message:
-          "Error interno del servidor, no se pudo traer los clientes del servidor",
-        error: error,
-      });
+    return res.status(500).json({
+      message:
+        "Error interno del servidor, no se pudo traer los clientes del servidor",
+      error: error,
+    });
   }
 });
 
@@ -110,13 +131,11 @@ app.get("/get-debts-client", async (req, res) => {
     const response2 = await clientSupabase.query(query2, [clientID]);
     const response1 = await clientSupabase.query(query1, [clientID]);
     if ((response3.rows && response1.rows) || response2.rows) {
-      return res
-        .status(200)
-        .json({
-          deudas: response1.rows,
-          entregas: response2.rows,
-          clientData: response3.rows,
-        });
+      return res.status(200).json({
+        deudas: response1.rows,
+        entregas: response2.rows,
+        clientData: response3.rows,
+      });
     } else {
       return res
         .status(400)
@@ -124,12 +143,10 @@ app.get("/get-debts-client", async (req, res) => {
     }
   } catch (error) {
     console.log(error);
-    return res
-      .status(500)
-      .json({
-        message:
-          "Error interno del servidor: no se pudo traer las deudas del servidor",
-      });
+    return res.status(500).json({
+      message:
+        "Error interno del servidor: no se pudo traer las deudas del servidor",
+    });
   }
 });
 
@@ -179,8 +196,7 @@ app.post("/add-debts", async (req, res) => {
 app.post("/make-deliver", async (req, res) => {
   const { clientID, clientName } = req.query;
   const { fecha_entrega, monto } = req.body;
-  const advanceOneMonth = dayjs(fecha_entrega)
-    .add(1, "month")
+  const advanceOneMonth = dayjs(fecha_entrega).add(1, "month");
 
   const formattMonth = dayjs(fecha_entrega).format("DD-MM-YYYY");
 
@@ -205,21 +221,17 @@ app.post("/make-deliver", async (req, res) => {
       return res.status(200).json({ message: "Entrega añadida exitosamente" });
     } else {
       await clientSupabase.query("ROLLBACK");
-      return res
-        .status(400)
-        .json({
-          message: "Hubo un problema al añadir la entrega, intente nuevamente",
-        });
+      return res.status(400).json({
+        message: "Hubo un problema al añadir la entrega, intente nuevamente",
+      });
     }
   } catch (error) {
     await clientSupabase.query("ROLLBACK");
     console.error("Error al guardar la entrega:", error);
-    res
-      .status(500)
-      .send({
-        message:
-          "Error interno del servidor: Error al guardar la entrega, intente nuevamente.",
-      });
+    res.status(500).send({
+      message:
+        "Error interno del servidor: Error al guardar la entrega, intente nuevamente.",
+    });
   }
 });
 
@@ -253,21 +265,17 @@ app.post("/cancel-debts", async (req, res) => {
       return res.status(200).send();
     } else {
       await clientSupabase.query("ROLLBACK");
-      return res
-        .status(400)
-        .json({
-          message: "Hubo un error al cancelar las deudas, intente nuevamente.",
-        });
+      return res.status(400).json({
+        message: "Hubo un error al cancelar las deudas, intente nuevamente.",
+      });
     }
   } catch (error) {
     await clientSupabase.query("ROLLBACK");
     console.error("Error al cancelar las deudas:", error);
-    res
-      .status(500)
-      .send({
-        message:
-          "Error interno del servidor: Error al cancelar las deudas, intente nuevamente.",
-      });
+    res.status(500).send({
+      message:
+        "Error interno del servidor: Error al cancelar las deudas, intente nuevamente.",
+    });
   }
 });
 
@@ -286,67 +294,101 @@ app.get("/get-history", async (req, res) => {
           .json({ message: "No se encontraron registros de este cliente" });
       }
     } catch (error) {
-      return res.status(500).json({message: "Error interno del servidor: No se pudo traer el historial del cliente", error})
+      return res
+        .status(500)
+        .json({
+          message:
+            "Error interno del servidor: No se pudo traer el historial del cliente",
+          error,
+        });
     }
-  }else{
-    return res.status(400).json({message: "El ID del cliente es requerido"})
+  } else {
+    return res.status(400).json({ message: "El ID del cliente es requerido" });
   }
 });
 
-app.get("/get-view-vencimientos", async(req,res)=>{
-  const query = 'SELECT * FROM vista_vencimientos'
+app.get("/get-view-vencimientos", async (req, res) => {
+  const query = "SELECT * FROM vista_vencimientos";
   try {
-    const response = await clientSupabase.query(query)
+    const response = await clientSupabase.query(query);
     if (response.rows) {
-      return res.status(200).send(response.rows)
-    }else{
-      return res.status(400).json({message: "No hay vencimientos para mostrar"})
+      return res.status(200).send(response.rows);
+    } else {
+      return res
+        .status(400)
+        .json({ message: "No hay vencimientos para mostrar" });
     }
   } catch (error) {
-    return res.status(500).json({message: "Error del servidor: No se pudieron traer los vencimientos, recargue la página para intentarlo nuevamente"})
+    return res
+      .status(500)
+      .json({
+        message:
+          "Error del servidor: No se pudieron traer los vencimientos, recargue la página para intentarlo nuevamente",
+      });
   }
-})
+});
 
 app.post("/create-clients", async (req, res) => {
   const { nombre_completo, apodo, dni, telefono } = req.body.values;
-  
-  const query1 = 'SELECT COUNT(*) AS existe FROM users WHERE LOWER(nombre_completo) = $1';
-  const query2 = 'SELECT COUNT(*) AS existe FROM users WHERE dni = $1';
-  const query3 = 'INSERT INTO users(nombre_completo, dni, telefono, apodo) VALUES($1, $2, $3, $4)';
-  
+
+  const query1 =
+    "SELECT COUNT(*) AS existe FROM users WHERE LOWER(nombre_completo) = $1";
+  const query2 = "SELECT COUNT(*) AS existe FROM users WHERE dni = $1";
+  const query3 =
+    "INSERT INTO users(nombre_completo, dni, telefono, apodo) VALUES($1, $2, $3, $4)";
+
   try {
-    const nombreExiste = await clientSupabase.query(query1, [nombre_completo.toLowerCase()]);
+    const nombreExiste = await clientSupabase.query(query1, [
+      nombre_completo.toLowerCase(),
+    ]);
     if (nombreExiste.rows[0].existe > 0) {
-      return res.status(400).json({ message: "El nombre completo ya existe, por favor intente con otro." });
+      return res
+        .status(400)
+        .json({
+          message: "El nombre completo ya existe, por favor intente con otro.",
+        });
     }
 
     const dniExiste = await clientSupabase.query(query2, [dni]);
     if (dniExiste.rows[0].existe > 0) {
-      return res.status(400).json({ message: "El DNI ya está registrado, por favor intente con otro." });
+      return res
+        .status(400)
+        .json({
+          message: "El DNI ya está registrado, por favor intente con otro.",
+        });
     }
 
     const response = await clientSupabase.query(query3, [
       nombre_completo.toLowerCase(),
       dni,
       telefono,
-      apodo
+      apodo,
     ]);
 
     if (response.rowCount > 0) {
       return res.status(200).send();
     } else {
-      return res.status(400).json({ message: "No se pudo crear el cliente, por favor intente nuevamente." });
+      return res
+        .status(400)
+        .json({
+          message: "No se pudo crear el cliente, por favor intente nuevamente.",
+        });
     }
   } catch (error) {
     console.error("Error en la creación del cliente:", error);
-    return res.status(500).json({ message: "Error interno del servidor: No se pudo crear el cliente, por favor intente nuevamente." });
+    return res
+      .status(500)
+      .json({
+        message:
+          "Error interno del servidor: No se pudo crear el cliente, por favor intente nuevamente.",
+      });
   }
 });
 
 app.delete("/delete-product", async (req, res) => {
   const idProduct = req.query.idProduct;
-  const query = 'DELETE FROM debts WHERE id = $1';
-  
+  const query = "DELETE FROM debts WHERE id = $1";
+
   try {
     await clientSupabase.query("BEGIN");
 
@@ -357,20 +399,26 @@ app.delete("/delete-product", async (req, res) => {
       return res.status(200).json({ message: "Producto eliminado con éxito" });
     } else {
       await clientSupabase.query("ROLLBACK");
-      return res.status(404).json({ message: "Producto no encontrado o ya eliminado" });
+      return res
+        .status(404)
+        .json({ message: "Producto no encontrado o ya eliminado" });
     }
   } catch (error) {
     await clientSupabase.query("ROLLBACK");
     console.error("Error al eliminar el producto:", error);
-    return res.status(500).json({ message: "Error interno del servidor: No se pudo eliminar el producto" });
+    return res
+      .status(500)
+      .json({
+        message: "Error interno del servidor: No se pudo eliminar el producto",
+      });
   }
 });
 
 app.delete("/delete-deliver", async (req, res) => {
   const idDeliver = req.query.idDeliver;
-  console.log(idDeliver)
+  console.log(idDeliver);
   const query = 'DELETE FROM "registerDelierys" WHERE id = $1';
-  
+
   try {
     await clientSupabase.query("BEGIN");
 
@@ -381,17 +429,20 @@ app.delete("/delete-deliver", async (req, res) => {
       return res.status(200).json({ message: "Entrega eliminada con éxito" });
     } else {
       await clientSupabase.query("ROLLBACK");
-      return res.status(404).json({ message: "Entrega no encontrado o ya eliminado" });
+      return res
+        .status(404)
+        .json({ message: "Entrega no encontrado o ya eliminado" });
     }
   } catch (error) {
     await clientSupabase.query("ROLLBACK");
     console.error("Error al eliminar la Entrega:", error);
-    return res.status(500).json({ message: "Error interno del servidor: No se pudo eliminar la Entrega" });
+    return res
+      .status(500)
+      .json({
+        message: "Error interno del servidor: No se pudo eliminar la Entrega",
+      });
   }
 });
-
-
-
 
 app.listen(process.env.PORT || 4000, () => {
   console.log("Servidor levantado en el puerto 4000");
